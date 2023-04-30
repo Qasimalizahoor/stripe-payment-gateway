8e2e6230672724db233bd25c7ed8bc4897110e69
Qasimalizahoor <Qasim3288@gmail.com>
2023-04-30 10:11:38 +0500

    Stripe Payment Route and Controller

---
+++ app/Http/Controllers/Stripe/StripePaymentController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Stripe;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Session;
+use Stripe;
+
+
+class StripePaymentController extends Controller
+{
+    public function stripe()
+    {
+        return view('stripe');
+    }
+
+    public function stripePost(Request $req)
+    {
+        Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
+        Stripe\Charge::create([
+            "amount" => 100* 100,
+            "currency"=> "usd",
+            "source"=>$req->StripeToken,
+            'description'=>"Test payment here"
+        ]);
+        Session::flash('success','Payment Successfuly');
+        return back();
+    }
+}
